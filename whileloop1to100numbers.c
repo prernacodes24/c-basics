3f56ed30e2e905b1bc26edb9bba1c1d7f0f4cc5c
prernacodes24 <kushprerna762@gmail.com>
2025-06-19 21:58:20 +0530

    Create whileloop1to100numbers.c

---
+++ whileloop1to100numbers.c
@@ -0,0 +1,15 @@
+//write a programm to print numbers 1 to 100 using while loop.
+
+#include <stdio.h>
+
+int main()
+{
+	int i=1;
+	while(i<=100)
+	{
+		printf("%d ",i);
+		i=i+1;
+	}
+
+	return 0;
+}
