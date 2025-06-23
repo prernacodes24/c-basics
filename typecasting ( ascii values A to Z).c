e799cf7efaf694de475a2ffd6f26ad018b583a8d
prernacodes24 <kushprerna762@gmail.com>
2025-06-23 20:26:00 +0530

    Create typecasting ( ascii values A to Z).c

---
+++ typecasting ( ascii values A to Z).c
@@ -0,0 +1,14 @@
+// typecasting 
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i=65;i<=90; i++)
+   { char ch = (char)i ; // this is called typecastin.
+     printf("%c ->",ch);
+     printf("%d\n",i);
+   }
+
+    return 0;
+}
